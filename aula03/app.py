fbb50202e073608fd87014d5e6ef047f80e4477b
KauanNaves <kauannazanaves0@gmail.com>
2025-11-09 14:27:09 -0300

    Aula 03 - Parte prática

---
+++ aula03/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# Definindo rota principal
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Definindo rota GET do formulário
+@app.route("/form")
+def form():
+    return render_template('form.html')
+
+# Definindo a rota PUT do formulário
+app.route('/dadosform', methods=["POST"])
+def dados_form():
+    nome = request.form['input-nome']
+    return render_template('resultado.html', nome=nome)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
